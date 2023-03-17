9238e519fc76e51141a69fe939d2c95c69e88409
ionwannabehere <24rodriguezd20@stu.smuhsd.org>
2023-03-17 19:16:46 +0000

    modified:   Test.py

--- Test.py
+++ Test.py
@@ -3,37 +3,51 @@ import numpy as np
 from pygame.locals import*
 
 
-width = 480
-height = 360
+width = 600
+height = 300
 screen_color = (49, 150, 100)
 line_color = (255, 0, 0)
 
 cubeA = [20,20,20,20,10,20,10,10,20,10,20,20,10,20,10,10,10,10,20,10,10,20,20,10]
+cubeALines = [0,1,1,2,2,3,3,4,4,5,5,6,6,7,0,7,0,3,4,7,1,6,2,5]
 cubeB = [20,20,20,20,20,0,20,0,0,20,0,20,0,20,20,0,20,0,0,0,0,0,0,20,]
+cubeBLines = [0,1,1,2,2,3,3,4,4,5,5,6,6,7]
 zNear = 50
 zFar = 100
 FovAngle = 100
 Aspect = height/width
 ƒ = (1/math.tan((FovAngle/2)* math.pi / 180))
+print(ƒ)
 
 PerProjMat = [[Aspect*ƒ,0,0,0],[0,ƒ,0,0],[0,0,(zFar/(zFar-zNear)),(((-1*zFar)*zNear)/(zFar-zNear))],[0,0,1,0]]
+RenderedPoints = []
 
-for i in range(8):
-    p = i*3
-    point = [cubeB[p],cubeB[p+1],cubeB[p+2],1]
-    mult = np.dot(PerProjMat, point)
-    print(str(mult) +  " : " + str(i+1))
-
+loadP = cubeA
+loadPLines = cubeALines
 
 def main():
     screen=pygame.display.set_mode((width,height))
     screen.fill(screen_color)
-
-    pygame.draw.line(screen,line_color, (60, 80), (130, 100))
-    pygame.draw.line(screen,line_color, (50, 50), (850, 400))
+    for i in range(8):
+        print(" ||||||| ******** ----------------------- ********** ||||||")
+        p = i*3
+        point = [loadP[p],loadP[p+1],loadP[p+2],1]
+        mult = np.dot(PerProjMat, point)
+        mult[1] = mult[1]/2
+        l=[]
+        for i in range(3):
+            l.append(((mult[i])/(mult[2])) * -1000 + 50)
+            print(l)
+        RenderedPoints.append(l)
+    for i in range(len(loadPLines)//2):
+        q = i*2
+        pygame.draw.line(screen,line_color, (int(RenderedPoints[loadPLines[q]][0]), int(RenderedPoints[loadPLines[q]][1])), (int(RenderedPoints[loadPLines[q+1]][0]), int(RenderedPoints[loadPLines[q+1]][1])))
     pygame.display.flip()
     while True:
-        for events in pygame.event.get():
-            if events.type == QUIT:
+        for event in pygame.event.get():
+            if event.type == QUIT:
                 sys.exit(0)
+            if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
+                if pygame.K_w:
+                    print("sadasd")
 main()
