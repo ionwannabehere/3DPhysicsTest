e0491bb1427706da1da252bdd4b84f013076e699
ionwannabehere <24rodriguezd20@stu.smuhsd.org>
2023-03-17 17:44:23 +0000

    modified:   Test.py

--- Test.py
+++ Test.py
@@ -1,14 +1,25 @@
-import sys, pygame
+import sys, pygame, math
 from pygame.locals import*
 
 width = 1000
 height = 500
 screen_color = (49, 150, 100)
 line_color = (255, 0, 0)
+cubeB = [20,20,20,20,20,0,20,0,0,20,0,20,0,20,20,0,20,0,0,0,0,0,0,20,]
+zNear = 10
+zFar = 75
+FovAngle = 100
+Aspect = height/width
+ƒ = (1/math.tan((FovAngle/2)))
+
+PerProjMat = [[Aspect*ƒ,0,0,0],[0,ƒ,0,0],[0,0,(zFar/(zFar-zNear)),(((-1*zFar)*zNear)/(zFar-zNear))],[0,0,1,0]]
+
+
 
 def main():
     screen=pygame.display.set_mode((width,height))
     screen.fill(screen_color)
+
     pygame.draw.line(screen,line_color, (60, 80), (130, 100))
     pygame.draw.line(screen,line_color, (50, 50), (850, 400))
     pygame.display.flip()
